e6c3c0f0f2c4b60fd7f9c86059bf679630090794
AliNurdinDvOPS <ali.nrdn14005@gmail.com>
2025-01-02 12:40:34 +0000

    Describe your changes

--- app/Http/Controllers/MuzakkiController.php
+++ app/Http/Controllers/MuzakkiController.php
@@ -124,7 +124,7 @@ class MuzakkiController extends Controller
                 'kategori_id' => $validatedData['kategori'][$key],
                 'type' => $validatedData['type'][$key],
                 'satuan' => $validatedData['satuan'][$key],
-                'created_by' => $validatedData['created_by'][$key], // Tambahkan ini
+                'created_by' => Auth::user()->role, // Tambahkan nilai created_by dari user yang login
             ]);
 
             $useHis = User::where('id', $user)->first();
